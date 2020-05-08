19a752471210c0ce989f233bc7da1854bc281539
leonardofrp5 <leonardofrp5@gmail.com>
2020-05-07 20:53:45 -0500

    test to promise

--- src/__test__/global.test.js
+++ src/__test__/global.test.js
@@ -10,3 +10,36 @@ test('prueba nombres', () => {
   expect(Json).toContain('Laura');
 });
 
+test('Mayor que', () => {
+  expect(9.1).toBeGreaterThan(9); 
+})
+
+test('Verdadero', () => {
+  expect(true).toBeTruthy();
+});
+
+const reverTring = (valor, callback) => {
+  callback(valor.split("").reverse().join(""));
+}
+
+test('Test Callback', () => {
+  reverTring('Hello', (valor) => {
+    expect(valor).toBe('olleH')
+  })
+});
+
+const reverseString2 = () => {
+  return new Promise((resolve, reject) => {
+    if(!str) {
+      reject(Error('Error'));
+    }
+    resolve(str.split("").reverse().join(""));
+  })
+};
+
+test('Prueba Callback', () => {
+  reverseString2('Hola')
+  .then(string => {
+    expect(string).toBe('aloH');
+  })
+})
