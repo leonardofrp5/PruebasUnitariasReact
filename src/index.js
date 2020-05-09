c0b0fa1d1f5f38a7d8b585cd9f08f674ea0668dc
leonardofrp5 <leonardofrp5@gmail.com>
2020-05-08 21:07:02 -0500

    make test whit array

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
+const cities = ['Cúcuta', 'Medllín', 'Santiago de Chile', 'Sao Paulo', 'Rio de Janeiro', 'Frutal'];
 
-   
+const randomCitires = () => {
+  return data = cities[Math.floor(Math.random() * cities.length)];
+}
+
+module.exports = randomCitires;
