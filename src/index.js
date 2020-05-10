e7aab4498f191cb18b731722abcb9f739644d020
leonardofrp5 <leonardofrp5@gmail.com>
2020-05-09 19:38:41 -0500

    use jest --coverage for reports

--- src/index.js
+++ src/index.js
@@ -4,4 +4,14 @@ const randomCitires = () => {
   return data = cities[Math.floor(Math.random() * cities.length)];
 }
 
-module.exports = randomCitires;
+const reverseString3 = (str) => {
+  return new Promise((resolve, reject) => {
+    if(!str) {
+      reject(Error('Error'));
+    }
+    resolve(str.split("").reverse().join(""));
+  });
+}
+
+module.exports.a = randomCitires;
+module.exports.b = reverseString3;
